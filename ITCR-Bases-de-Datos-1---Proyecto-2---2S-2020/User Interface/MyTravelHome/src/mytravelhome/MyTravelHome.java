ec7d80cc20bcd1317de2488a13e276b3949d2533
RonaldoVindas <44733416+RonaldoVindas@users.noreply.github.com>
2021-01-16 19:54:22 -0600

    Partial UI created... more info soon

---
+++ ITCR-Bases-de-Datos-1---Proyecto-2---2S-2020/User Interface/MyTravelHome/src/mytravelhome/MyTravelHome.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytravelhome;
+
+import UIFrames.Login_Menu;
+
+/**
+ *
+ * @author rony1
+ */
+public class MyTravelHome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Login_Menu menu = new Login_Menu();
+       menu.setVisible(true);
+    }
+    
+}
